c69c2aa583af130a2aa78db946ea67e10f8933ee
AsmodeusXI <AsmodeusXI@gmail.com>
2018-05-03 22:19:25 -0400

    feat: remove kruft

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,5 @@
 import { MP_BY_TYPE } from './constants';
 
-export function generateMP() {
-  return 'Hello World';
-}
-
 export function getMPForType(type1: string, type2: string = null): number {
   return MP_BY_TYPE[type1][`${type2 ? type2 : type1}`];
 }
-
-console.log(getMPForType('Normal', 'Ice'));
