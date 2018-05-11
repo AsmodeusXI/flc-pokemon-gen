f5a15036c2c93aa1f4365b8c8cbda2ddc6769110
AsmodeusXI <AsmodeusXI@gmail.com>
2018-05-10 22:19:08 -0400

    feat: additional tests and getMPForPokemonMove

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -5,5 +5,5 @@ test("getMPForType fetches the expected values", () => {
   expect(index.getMPForType('Fighting', 'Fairy')).toEqual(112);
   expect(index.getMPForType('Electric', 'Poison')).toEqual(106);
   expect(index.getMPForType('Psychic', 'Light')).toEqual(106);
-  expect(index.getMPForType('Normal')).toEqual(124);
+  expect(index.getMPForType('Steel', 'Grass')).toEqual(88);
 });
--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,44 @@ import { MP_BY_TYPE } from './constants';
 export function getMPForType(type1: string, type2: string = null): number {
   return MP_BY_TYPE[type1][`${type2 ? type2 : type1}`];
 }
+
+enum MoveStatus {
+  BUFF,
+  CONTROL,
+  DOT,
+  CLEAN,
+  ALLY,
+  ENEMY
+}
+
+interface PokemonMove {
+  type: string,
+  power?: number,
+  status?: MoveStatus
+}
+
+interface Pokemon {
+  type1: string,
+  type2: string,
+  moves: Array<PokemonMove>
+}
+
+export function getMPForPokemonMove(move: PokemonMove, pokemon: Pokemon): number {
+  let moveMP = 6; // Starting value.
+  // In-Type Costs
+  if (move.power && !move.status) {
+    moveMP += move.power;
+  } else {
+    moveMP += 10;
+    const moveStatuses = pokemon.moves.map((currentMoves) => {
+      if (currentMoves.status) return currentMoves.status;
+    });
+    const uniqueStatuses = [move.status];
+    moveStatuses.forEach((moveStatus) => {
+      if (!uniqueStatuses.includes(moveStatus)) uniqueStatuses.push(moveStatus);
+    });
+    moveMP += (uniqueStatuses.length - 1) * 10;
+  }
+  // Out-Type Costs
+  return moveMP;
+}
